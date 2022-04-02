16d8fae03a27e56c613f67d9ddcdd21757c71fe8
yjiang <907292671@qq.com>
2022-04-02 18:43:28 +0800

    binary tree

--- src/class00_20220402/BinaryTree.java
+++ src/class00_20220402/BinaryTree.java
@@ -1,6 +1,6 @@
 package class00_20220402;
 
-import java.util.LinkedList;
+import java.util.*;
 
 public class BinaryTree {
     public static void print(Node head){
@@ -9,16 +9,46 @@ public class BinaryTree {
         }
         LinkedList<Node> list = new LinkedList<>();
         list.add(head);
+        Node curEndNode = head;
+        Node nextEndNode = null;
+        int layer = 0;
+        Map<Integer, String> printList = new LinkedHashMap<>();
         while(!list.isEmpty()){
             Node node = list.poll();
-            System.out.print(node.value + " ");
-            if(node.left != null) list.add(node.left);
-            if(node.right != null) list.add(node.right);
+            printList.put(layer, printList.containsKey(layer) ? printList.get(layer) + " " + node.value :"" + node.value);
+            if(node.left != null) {
+                list.add(node.left);
+                nextEndNode = node.left;
+            }
+            if(node.right != null){
+                list.add(node.right);
+                nextEndNode = node.right;
+            }
+            if(curEndNode == node){
+                layer ++;
+                curEndNode = nextEndNode;
+            }
+        }
+
+        for (Map.Entry<Integer, String> integerStringEntry : printList.entrySet()) {
+            for (int i = 0; i < Math.pow(2, printList.size()-integerStringEntry.getKey())/2; i++) {
+                System.out.print(" ");
+            }
+            System.out.println(integerStringEntry.getValue());
         }
     }
 
-    public static void generateRandomTree(int level, int maxValue){
-        Node head = new Node((int)(maxValue*Math.random()));
 
+    public static Node generateRandomNode(int level, int maxValue){
+        Node head = new Node((int)(maxValue*Math.random()));
+        if(level == 0) return head;
+        head.left = generateRandomNode(level-1, maxValue);
+        head.right = generateRandomNode(level-1, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+//        PrintBT.printTree(generateRandomNode(5, 100));
+        print(generateRandomNode(5, 100));
     }
 }
