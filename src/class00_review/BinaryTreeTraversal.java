f3124a5e84b867a039c4952956a1270040bcf741
yjiang <907292671@qq.com>
2022-07-13 20:20:36 +0800

    all changed

--- src/class00_review/BinaryTreeTraversal.java
+++ src/class00_review/BinaryTreeTraversal.java
@@ -23,6 +23,24 @@ public class BinaryTreeTraversal {
         return root;
     }
 
+    public static BinaryTreeNode generateTree(int n){
+        if(n <= 0){
+            return null;
+        }
+        BinaryTreeNode[] nodes = new BinaryTreeNode[n];
+        for (int i = 0; i < nodes.length; i++) {
+            nodes[i] = new BinaryTreeNode(i+1);
+        }
+
+        int index = 0;
+        while(index < n){
+            if(2*index + 1 < n) nodes[index].left = nodes[2*index + 1];
+            if(2*index + 2 < n) nodes[index].right = nodes[2*index + 2];
+            index ++;
+        }
+        return nodes[0];
+    }
+
     // 递归序推导出来
     public static void recursionOrder(BinaryTreeNode node, int order){
         if(node == null){
@@ -57,19 +75,62 @@ public class BinaryTreeTraversal {
     }
 
     public static void inOrder(BinaryTreeNode node){
+        if(node == null){
+            return;
+        }
         LinkedList<BinaryTreeNode> stack = new LinkedList<>();
-        stack.push(node);
-        while(!stack.isEmpty()){
-            BinaryTreeNode n = stack.pop();
-            System.out.print(n.val + " ");
-            if(n.right != null){
-                stack.push(n.right);
-            }
-            if(n.left != null){
-                stack.push(n.left);
+        while(!stack.isEmpty() || node != null){
+            if(node!=null){
+                stack.push(node);
+                node = node.left;
+            }else{
+                node = stack.pop();
+                System.out.print(node.val + " ");
+                node = node.right;
             }
         }
     }
+
+    // 先序遍历压栈，后出栈
+    public static void posOrder(BinaryTreeNode node){
+        LinkedList<BinaryTreeNode> preStack = new LinkedList<>();
+        LinkedList<BinaryTreeNode> posStack = new LinkedList<>();
+        preStack.push(node);
+        BinaryTreeNode cur;
+        while(!preStack.isEmpty()){
+            cur = preStack.pop();
+            posStack.push(cur);
+            if(cur.left != null){
+                preStack.push(cur.left);
+            }
+            if(cur.right != null){
+                preStack.push(cur.right);
+            }
+        }
+
+        while(!posStack.isEmpty()){
+            System.out.print(posStack.pop().val + " ");
+        }
+    }
+
+    public static void posOrder2(BinaryTreeNode head){
+        LinkedList<BinaryTreeNode> posStack = new LinkedList<>();
+        BinaryTreeNode prePopNode = head;
+        posStack.push(prePopNode);
+        while(!posStack.isEmpty()){
+            BinaryTreeNode cur = posStack.peek();
+            if(cur.left != null && cur.left != prePopNode && cur.right != prePopNode){
+                posStack.push(cur.left);
+            }else if(cur.right != null && cur.right != prePopNode){
+                posStack.push(cur.right);
+            }else{
+                System.out.print(posStack.pop().val + " ");
+                prePopNode = cur;
+            }
+        }
+    }
+
+
 
     public static void main(String[] args) {
         BinaryTreeNode treeNode = getSample();
@@ -85,6 +146,18 @@ public class BinaryTreeTraversal {
 
         System.out.println("------preOrder stack--------");
         preOrder(treeNode);
+        System.out.println();
+
+        System.out.println("------inOrder stack--------");
+        inOrder(treeNode);
+        System.out.println();
+
+        System.out.println("------posOrder stack--------");
+        posOrder(treeNode);
+        System.out.println();
+
+        System.out.println("------posOrder stack2--------");
+        posOrder2(treeNode);
         System.out.println();
     }
 
