20692013c965a66bfc67c0a35d22238326697a03
yjiang <907292671@qq.com>
2022-07-13 18:32:10 +0800

    leetcode 188

---
+++ src/class00_20220713/Solution.java
@@ -0,0 +1,54 @@
+package class00_20220713;
+
+import java.util.Arrays;
+
+/**
+ * 188. 买卖股票的最佳时机 IV
+ * 给定一个整数数组 prices ，它的第 i 个元素 prices[i] 是一支给定的股票在第 i 天的价格。
+ *
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。
+ *
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ */
+public class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int[][] res = new int[prices.length+1][k+1];
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res[i].length; j++) {
+                res[i][j] = -1;
+            }
+        }
+        int ans = process(prices, 0, k, res);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(Arrays.toString(res[i]));
+        }
+        return ans;
+    }
+
+    public int process(int[] prices, int index, int k, int[][] res){
+        if(res[index][k] != -1){
+            return res[index][k];
+        }
+
+        if(index==prices.length || k == 0){
+            res[index][k] = 0;
+            return 0;
+        }
+
+        int notBuyRes = process(prices, index + 1, k, res);
+
+        int buyRes = 0;
+        for (int i = index+1; i < prices.length; i++) {
+            buyRes = Math.max(buyRes, prices[i]-prices[index] + process(prices, i+1, k-1, res));
+        }
+
+        int ans = Math.max(notBuyRes, buyRes);
+        res[index][k] = ans;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        System.out.println(obj.maxProfit(2, new int[]{2, 4, 1}));
+    }
+}
