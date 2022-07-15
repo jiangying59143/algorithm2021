fef5c3732f399effc4dcd73955162452e9a6ca47
yjiang <907292671@qq.com>
2022-07-15 18:01:03 +0800

    practice

--- src/class01/Practice.java
+++ src/class01/Practice.java
@@ -39,6 +39,41 @@ public class Practice {
     }
 
 
+    public static void mergeSort(int[] arr){
+        if(arr == null || arr.length < 2){
+            return;
+        }
+
+        int[] helpArr = new int[arr.length];
+        processMergeSort(arr, 0, arr.length-1, helpArr);
+        for (int i = 0; i < helpArr.length; i++) {
+            arr[i] = helpArr[i];
+        }
+    }
+
+    public static void processMergeSort(int[] arr, int L, int R, int[] helpArr){
+        if(L <= R) return;
+        int mid = L + ((R-L)>>1);
+        processMergeSort(arr, L, mid, helpArr);
+        processMergeSort(arr, mid+1, R, helpArr);
+        merge(arr, L, mid, R, helpArr);
+    }
+
+    private static void merge(int[] arr, int L, int mid, int R, int[] helpArr) {
+        int i = L;
+        int j = mid+1;
+        int helpIndex = L;
+        while(i <= mid && j <= R){
+            helpArr[helpIndex++] = arr[i] <= arr[j] ?  arr[i++] : arr[j++];
+        }
+        while(i<=mid){
+            helpArr[helpIndex++] = arr[i++];
+        }
+        while(j <= R){
+            helpArr[helpIndex++] = arr[j++];
+        }
+    }
+
 
     public static void swap(int[] arr, int i, int j){
         int temp = arr[i];
